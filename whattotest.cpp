5dedb92450e1925d858a5c512a9c3936f2187e10
muhlenlogan8 <muhlenlogan8@gmail.com>
2023-11-27 18:15:01 -0500

    Initial commit

---
+++ whattotest.cpp
@@ -0,0 +1,59 @@
+// whattotest.cpp
+
+#include "whattotest.h"
+ 
+WidgetWithArrays::WidgetWithArrays() : m_count(0)
+{
+    for (int i =0 ; i < 100; i++) 
+    {
+        m_array[i] = 0;
+    }
+}
+int num = 0;
+void WidgetWithArrays::storeInteger(int a)
+{
+    m_array[num] = a;
+    num++;
+}
+
+int WidgetWithArrays::sum()
+{
+    int retVal = 0;
+
+    // hint standard for loop OR
+    // try looking into range based for loop (hint it can work with dumb arrays)
+    for (int n : m_array)
+    {
+        retVal += n;
+    }
+
+    return retVal;
+}
+ 
+
+WidgetWithVector::WidgetWithVector()
+{
+
+
+}
+
+void WidgetWithVector::storeInteger(int a)
+{
+    m_vector.push_back(a);
+
+}
+
+int WidgetWithVector::sum()
+{
+    int retVal = 0;
+
+    for(int n : m_vector)
+    {
+        retVal = retVal +n;
+    }
+    
+    return retVal;
+}
+ 
+
+
